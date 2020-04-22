5cd8ab56d6a6061f132ecd22d8499e807d34746f
Katie Wilday <wildayk@gmail.com>
2020-04-22 15:56:35 -0400

    first commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { Menu, Message } from 'semantic-ui-react'
+
+export default class Nav extends Component {
+    constructor(){
+        super()
+
+        this.state={
+            activeItem: 'Explore'
+        }
+    }
+
+    handleItemClick = (event, {name}) => {
+        this.setState({ activeItem: name})
+    }
+
+
+    render() {
+        const { activeItem } = this.state
+
+            return(
+            <React.Fragment>
+                <Menu pointing>
+                    <Menu.Item
+                        position='right'
+                        name='Create'
+                        active={activeItem === 'Create'}
+                        onClick={this.handleItemClick}
+                        href="/crawls/new"
+                    />
+                    <Menu.Item
+                        position='right'
+                        name='Explore'
+                        active={activeItem === 'Explore'}
+                        onClick={this.handleItemClick}
+                        href="/crawls"
+                    />
+                    <Menu.Item
+                        position='right'
+                        name='Settings'
+                        active={activeItem === 'Settings'}
+                        onClick={this.handleItemClick}
+                        href="/settings"
+                    />
+                </Menu>
+            </React.Fragment>
+            )
+        }
+}
